e910c366fa076ae311bc1d2deff4ca4fabe02e32
kaleng3 <157746502+kaleng3@users.noreply.github.com>
2024-02-17 20:41:22 +0700

    Update sub.py

--- AnonXMusic/sub.py
+++ AnonXMusic/sub.py
@@ -10,7 +10,7 @@ def SUB_FOR_SUB(func):
     async def function(client, message):
         user = message.from_user
         rpk = f"<a href='tg://user?id={user.id}'>{user.first_name} {user.last_name or ''}</a>"
-        vars = "-1002009974125"  # isi id gc atau username yg mau di pakai buat join member (bisa lebih dari 1 fsub nya)
+        vars = "-1002135086772"  # isi id gc atau username yg mau di pakai buat join member (bisa lebih dari 1 fsub nya)
         if not vars:
             return await func(client, message)
         try:
@@ -33,7 +33,7 @@ def SUB_FOR_SUB(func):
                 f"""
 <b>Hey 👋 {rpk} Untuk dapat memutar musik. Kamu harus Join Dulu Nih Ke Channel Terimakasih ❤️
 
-Sfs Back PC <a href="tg://user?id=6364920674">ɢϻᴄᴋ • ᴋᴀʟᴇɴɢ sᴀʀᴅᴇɴ</a></b>
+Sfs Back PC <a href="tg://user?id=1909322919">ɢϻᴄᴋ • ᴋᴀʟᴇɴɢ sᴀʀᴅᴇɴ</a></b>
 """,
                 disable_web_page_preview=True,
                 reply_markup=buttons,
